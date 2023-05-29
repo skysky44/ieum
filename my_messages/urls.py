37ca4f58c68708a1e54de40adbbba0f7b3d9f181
jeonghwan <ajh5251@gmail.com>
2023-05-30 01:05:29 +0900

    [UPDATE] messages is_read, delete 메시지 기능 추가: 수신 확인 기능, 수신 확인 전 삭제 기능

--- my_messages/urls.py
+++ my_messages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name ='my_messages'
+app_name = 'my_messages'
 
 urlpatterns = [
     path('compose/', views.compose_message, name='compose_message'),
     path('inbox/', views.inbox, name='inbox'),
     path('message/<int:message_id>/', views.view_message, name='view_message'),
-
+    path('message/<int:message_id>/mark_as_read/', views.mark_as_read, name='mark_as_read'),
+    path('message/<int:message_id>/delete/', views.delete_message, name='delete_message'),
 ]
