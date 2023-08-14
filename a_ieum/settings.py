6d714c4a1d63113e8af51c15c64b9822e41667b6
jeonghwan <ajh5251@gmail.com>
2023-08-15 00:38:43 +0900

    [UPDATE] aws test

--- a_ieum/settings.py
+++ a_ieum/settings.py
@@ -28,8 +28,8 @@ load_dotenv()
 CLIENT_ID = os.getenv('client_id')
 CLIENT_SECRET = os.getenv('client_secret')
 KAKAO_API_KEY = os.getenv('KAKAO_API_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-m-0!*yv=-wb#wq)80ix=3mk&xagc8ufs!l(^1!oqhxve@pa82y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
